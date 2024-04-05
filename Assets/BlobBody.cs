e6557929b0ea0e0fd55230b80d82d795d363ecda
Zolteks <96941840+Zolteks@users.noreply.github.com>
2024-04-05 15:01:39 +0200

    shape wip

--- Assets/BlobBody.cs
+++ Assets/BlobBody.cs
@@ -8,14 +8,14 @@ public class BlobBody : MonoBehaviour
     GameObject[] baseShape;
     GameObject[] curShape;
     [Range(0, 1)] public float springRestLength;
-    [Range(1, 10)] public float springPower;
+    [Range(10, 100)] public float springPower;
 
     void Start()
     {
-        baseShape = GameObject.FindGameObjectsWithTag("Blob Body Part");
-        curShape = baseShape;
+        baseShape = GameObject.FindGameObjectsWithTag("Base Shape Part");
+        curShape = GameObject.FindGameObjectsWithTag("Blob Body Part");
         springRestLength = 1;
-        springPower = 0;
+        springPower = 10;
     }
 
     void Update()
@@ -23,15 +23,40 @@ public class BlobBody : MonoBehaviour
         
     }
 
+    Vector2 GetBarycenter(ref GameObject[] Lpoint)
+    {
+        float totalX = 0;
+        float totalY = 0;
+        foreach (GameObject curPoint in Lpoint)
+        {
+            totalX += curPoint.transform.position.x;
+            totalY += curPoint.transform.position.y;
+        }
+
+        return new Vector2(totalX / Lpoint.Length, totalY / Lpoint.Length);
+    }
+
     private void FixedUpdate()
     {
+        Vector2 curBarycenter = GetBarycenter(ref curShape);
+
+        float curX = GetComponent<Transform>().transform.position.x;
+        float curY = GetComponent<Transform>().transform.position.y;
+        
+        //GameObject.FindGameObjectWithTag("Base Shape").gameObject.transform.position = GetComponent<Transform>().transform.position;
+
+        //GetComponent<Transform>().transform.position = curBarycenter;
+
         for (int i = 0; i < curShape.Length; i++)
         {
-            float epe = (float)(0.5 * springPower * Math.Pow(springRestLength, 2));
+            Vector2 absolute = new Vector2(curX + curShape[i].GetComponent<Rigidbody2D>().transform.rotation.x, curY + curShape[i].GetComponent<Rigidbody2D>().transform.rotation.y);
+            //float epe = (float)(0.5 * springPower * Math.Pow(springRestLength, 2));
 
             Vector2 recalibrate = new Vector2(baseShape[i].transform.position.x - curShape[i].transform.position.x, baseShape[i].transform.position.y - curShape[i].transform.position.y);
 
-            GetComponent<Rigidbody2D>().AddForce(recalibrate * epe);
+            float epe = 1 / springPower;
+
+            //curShape[i].GetComponent<Rigidbody2D>().AddForce(recalibrate/2);
         }
     }
 }
