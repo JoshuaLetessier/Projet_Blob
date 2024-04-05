74d7be1d1df2eb7bf24f6eafcfd626b54ae8d1c1
Riuck01 <117281231+Riuck01@users.noreply.github.com>
2024-04-05 14:50:09 +0200

    mouv incoming

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class test : MonoBehaviour
 {
     public int moveSpeed;
+    public int slideSpeed;
     public int jumpPower;
+    private bool isWallSliding;
+    private float wallSlidingSpeed = 2f;
+    private Transform wallCheck;
 
     public PlayerFeet playerFeet;
+    public LeftWallJump leftWallJump;
+    public RightWallJump rightWallJump;
 
     // Start is called before the first frame update
     void Start()
     {
         moveSpeed = 10;
-        jumpPower = 750;
+        slideSpeed = 5;
+        jumpPower = 500;
     }
 
     // Update is called once per frame
@@ -32,9 +39,20 @@ public class test : MonoBehaviour
 
         GetComponent<Rigidbody2D>().velocity = curVelocity;
 
-        if (Input.GetKeyDown(KeyCode.Space) && playerFeet.isGrounded == true)
+        if (Input.GetKeyDown(KeyCode.Space) && (playerFeet.isGrounded == true))
         {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1) * jumpPower);
         }
+
+/*        else if (Input.GetKeyDown(KeyCode.Space) &&  leftWallJump.isLeftWall)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(5 += GetComponent<Rigidbody2D>().velocity.x, 2/3) * jumpPower);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Space) && rightWallJump.isRightWall)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-5 - GetComponent<Rigidbody2D>().velocity.x, 2/3) * jumpPower);
+        }*/
+
     }
 }
