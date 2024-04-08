6576904f5df15a4285ee04780e860de9e42a3fd4
Riuck01 <117281231+Riuck01@users.noreply.github.com>
2024-04-08 17:00:36 +0200

    TEST 2

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -7,53 +7,65 @@ public class test : MonoBehaviour
     public int moveSpeed;
     public int slideSpeed;
     public int jumpPower;
-    private bool isWallSliding;
     private float wallSlidingSpeed = 2f;
-    private Transform wallCheck;
+
+    private bool disableA = true;
+    private bool disableD = true;
 
     public PlayerFeet playerFeet;
     public LeftWallJump leftWallJump;
     public RightWallJump rightWallJump;
 
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+
     void Start()
     {
         moveSpeed = 10;
         slideSpeed = 5;
         jumpPower = 500;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector2 curVelocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+        Vector2 curVelocity = new Vector2(0, rb.velocity.y);
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && disableA)
         {
             curVelocity.x -= moveSpeed;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && disableD)
         {
             curVelocity.x += moveSpeed;
         }
 
-        GetComponent<Rigidbody2D>().velocity = curVelocity;
+        rb.velocity = curVelocity;
 
-        if (Input.GetKeyDown(KeyCode.Space) && (playerFeet.isGrounded == true))
+        if (Input.GetKeyDown(KeyCode.Space) && playerFeet.isGrounded)
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1) * jumpPower);
+            rb.AddForce(Vector2.up * jumpPower);
         }
 
-/*        else if (Input.GetKeyDown(KeyCode.Space) &&  leftWallJump.isLeftWall)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(5 += GetComponent<Rigidbody2D>().velocity.x, 2/3) * jumpPower);
+            if (leftWallJump.isLeftWall)
+            {
+                rb.AddForce(new Vector2(jumpPower, jumpPower * 1 / 2));
+            }
+            else if (rightWallJump.isRightWall)
+            {
+                rb.AddForce(new Vector2(-jumpPower, jumpPower * 1/2));
+            }
         }
 
-        else if (Input.GetKeyDown(KeyCode.Space) && rightWallJump.isRightWall)
+        bool isWallSliding = (leftWallJump.isLeftWall || rightWallJump.isRightWall) && !playerFeet.isGrounded;
+        if (isWallSliding)
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(-5 - GetComponent<Rigidbody2D>().velocity.x, 2/3) * jumpPower);
-        }*/
-
-
+            //disableA = false;
+            //disableD = false;
+            rb.velocity = new Vector2(rb.velocity.x, -wallSlidingSpeed);
+        }
     }
+
+
 }
